9facc88af202ef2f81fc60a230cd76202eabfa91
oaktreenum <63089531+oaktreenum@users.noreply.github.com>
2022-09-23 13:43:17 -0400

    Update sketch.js

--- porthead/sketch.js
+++ porthead/sketch.js
@@ -1,9 +1,10 @@
 function preload() {
   // preload() runs once
-bkg = loadImage('assets/bkg.jpg');
+  bkg = loadImage('assets/bkg.jpg');
+
   cimg = loadImage('assets/d.svg');
   dimg = loadImage('assets/c.svg');
-  eimg = loadImage('assets/ef.svg');
+  eimg = loadImage('assets/e.svg');
 }
 
 function setup() {
@@ -16,6 +17,12 @@ function setup() {
   hRatio = height/bkg.height;
   rat  = max(wRatio,hRatio);
   bkg.resize(bkg.width*rat,bkg.height*rat);
+  
+  if(innerWidth<500){
+    cimg.resize(65,67);
+    dimg.resize(65,67);
+    eimg.resize(65,67);
+  }
 }
 
 function draw() {
@@ -37,14 +44,32 @@ function draw() {
   a=180;
   b=60;
   c=30;
-  if(innerWidth<800){
+  d=145;
+  e=150;
+  
+  if(width<800){
     a=a/2;
     b=b/2;
     c=c/2;
   } else{
       a=180;
       b=60;
-      c=30;}
+      c=30;
+  }
+  
+  if(width<500){
+    d=d/2;
+    e=c/2;
+    image(dimg,innerWidth-d,innerHeight/7);
+    image(eimg,innerWidth-d,3*innerHeight/7);
+    image(cimg,innerWidth-75,5*innerHeight/7);
+  }else{
+    d=145;
+    e=150;
+    image(dimg,innerWidth-d,innerHeight/7);
+    image(eimg,innerWidth-d,3*innerHeight/7);
+    image(cimg,innerWidth-150,5*innerHeight/7);
+  }
   
   textFont('Silkscreen');
   textSize(b);
@@ -61,15 +86,9 @@ function draw() {
   textSize(13);
   text("LinkedIn", innerWidth/4-31, 26);
   textSize(13);
-  text("Instagram", 2*innerWidth/4-31, 26);
+  text("Instagram", 2*innerWidth/4-45, 26);
   textFont('Space Mono');
   textSize(13);
   text("Quantum Crypto", 3*innerWidth/4-52, 26);
- 
-  image(dimg,innerWidth-145,innerHeight/7);
-  image(eimg,innerWidth-145,3*innerHeight/7);
-  image(cimg,innerWidth-150,5*innerHeight/7);
-  
-  
-  
+
 }
